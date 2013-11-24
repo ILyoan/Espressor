6468c68951f32fcb0a16f757c902015278a3b291
ILyoan <ilyoan@gmail.com>
2013-11-24 17:28:53 +0900

    WIP: Rename & Reorder based on ECMA specfication

--- src/grinder/parser.rs
+++ src/grinder/parser.rs
@@ -36,27 +36,41 @@ impl Parser {
         }
     }
 
+
+    // ECMA 11.13 Assignment Operators
+    fn parse_assignment_expression(&mut self) {
+        fail!("Not Implemented")
+    }
+
+    // ECMA 11.14 Comma Operator (,)
+    fn parse_expression(&mut self) {
+        self.parse_assignment_expression();
+        fail!("Not Implemented");
+    }
+
+
+    // ECMA 12 Statement
     fn parse_statement(&mut self) {
         // FIXME: remove copy.
         let token = self.token.clone();
         match token {
-            token::SEMICOLON => self.parse_stmt_empty(),
-            token::LBRACE => self.parse_stmt_block(),
-            token::LPAREN => self.parse_stmt_expression(),
+            token::SEMICOLON => self.parse_empty_statement(),
+            token::LBRACE => self.parse_block(),
+            token::LPAREN => self.parse_expression_statement(),
             token::IDENT(ref ident) => {
                 match ident.as_slice() {
-                    "if" => self.parse_stmt_if(),
-                    "break" => self.parse_stmt_break(),
-                    "continue" => self.parse_stmt_continue(),
-                    "with" => self.parse_stmt_with(),
-                    "switch" => self.parse_stmt_switch(),
-                    "return" => self.parse_stmt_return(),
-                    "throw" => self.parse_stmt_throw(),
-                    "try" => self.parse_stmt_try(),
-                    "while" => self.parse_stmt_while(),
-                    "do" => self.parse_stmt_do_while(),
-                    "for" => self.parse_stmt_for(),
-                    "var" => self.parse_stmt_declaration(),
+                    "if" => self.parse_if_statement(),
+                    "do" => self.parse_do_while_statement(),
+                    "while" => self.parse_while_statement(),
+                    "for" => self.parse_for_statement(),
+                    "continue" => self.parse_continue_statement(),
+                    "break" => self.parse_with_statement(),
+                    "return" => self.parse_return_statement(),
+                    "with" => self.parse_with_statement(),
+                    "switch" => self.parse_switch_statement(),
+                    "throw" => self.parse_throw_statement(),
+                    "try" => self.parse_try_statement(),
+                    "var" => self.parse_variable_statment(),
                     _ => fail!("Not Implemented")
                 }
             }
@@ -64,67 +78,99 @@ impl Parser {
         }
     }
 
-    fn parse_stmt_empty(&mut self) {
+    // ECMA 12.1 Block
+    fn parse_block(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_block(&mut self) {
+    fn parse_statmemt_list(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_expression(&mut self) {
+    // ECMA 12.2 Variable Statement
+    fn parse_variable_statment(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_if(&mut self) {
+    // ECMA 12.3 Empty Statement
+    fn parse_empty_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_break(&mut self) {
+    // ECMA 12.4 Expression Statement
+    fn parse_expression_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_continue(&mut self) {
+    // ECMA 12.5 if Statement
+    fn parse_if_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_with(&mut self) {
+    // ECMA 12.6 Iteration Statement
+
+    // ECMA 12.6.1 do-while Statement
+    fn parse_do_while_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_switch(&mut self) {
+    // ECMA 12.6.2 while Statement
+    fn parse_while_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_return(&mut self) {
+    // ECMA 12.6.3 for Statement
+    fn parse_for_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_throw(&mut self) {
+    // ECMA 12.6.4 for-in Statement
+    fn parse_for_in_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_try(&mut self) {
+    // ECMA 12.7 continue Statement
+    fn parse_continue_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_while(&mut self) {
+    // ECMA 12.8 break Statement
+    fn parse_break_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_do_while(&mut self) {
+    // ECMA 12.9 return Statement
+    fn parse_return_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_for(&mut self) {
+    // ECMA 12.10 with Statement
+    fn parse_with_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_expression(&mut self) {
+    // ECMA 12.11 switch Statement
+    fn parse_switch_statement(&mut self) {
         fail!("Not Implemented");
     }
 
-    fn parse_stmt_declaration(&mut self) {
+    // ECMA 12.12 Labelled Statement
+    fn parse_labelled_statement(&mut self) {
+        fail!("Not Implemented");
+    }
+
+    // ECMA 12.13 throw Statement
+    fn parse_throw_statement(&mut self) {
+        fail!("Not Implemented");
+    }
+
+    // ECMA 12.14 try Statement
+    fn parse_try_statement(&mut self) {
+        fail!("Not Implemented");
+    }
+
+    // ECMA 12.15 debugger Statement
+    fn parse_debugger_statement(&mut self) {
         fail!("Not Implemented");
     }
 
